062ecc95eb9ad596840f180cfc2c25c1e97618b6
Awesome <awesome_devnet@outlook.com>
2024-02-05 17:56:28 +0900

    feat: compile shader

--- MotionGL_Gyro/MotionGL_Gyro.cpp
+++ MotionGL_Gyro/MotionGL_Gyro.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 
+unsigned int compileShader(const char* v, const char* f);
+
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -36,7 +38,26 @@ int main() {
     }
 
     // TODO: Set the code that is related to shader compilation and setting
+    // Vertex Shader
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "}\0";
 
+    // Fragment Shader
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+
+    // Compile the shader
+    unsigned int shaderProgram = compileShader(vertexShaderSource, fragmentShaderSource);
+    
+    // Render loop
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -53,6 +74,29 @@ int main() {
     // Terminate GLFW
     glfwTerminate();
     return 0;
+}
+
+// Compile the shader
+unsigned int compileShader(const char* v, const char* f) {
+    unsigned int vertexShader, fragmentShader, shaderProgram;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &v, NULL);
+    glCompileShader(vertexShader);
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &f, NULL);
+    glCompileShader(fragmentShader);
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(shaderProgram);
+    return shaderProgram;
 }
 
 void processInput(GLFWwindow* window) {
